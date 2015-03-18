165e4757f659d845609e1165797f2ef0f1fd7fd0
piglikeyoung <316046510@qq.com>
2015-03-18 09:12:33 +0800

    Initial Commit

---
+++ 彩票(lottery)/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  彩票(lottery)
+//
+//  Created by piglikeyoung on 15/3/18.
+//  Copyright (c) 2015年 jinheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 彩票(lottery)/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  彩票(lottery)
+//
+//  Created by piglikeyoung on 15/3/18.
+//  Copyright (c) 2015年 jinheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
